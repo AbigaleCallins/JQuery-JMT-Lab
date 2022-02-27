0403c27542e69b7fefa3371981950c6639ef19dd
AbigaleCallins <alcallins@gmail.com>
2022-02-27 00:04:19 -0600

    final

--- scripts.js
+++ scripts.js
@@ -1,62 +1,92 @@
 $(document).ready(function () {
-  let sentences = [
-    "ten ate neite ate nee enet ite ate inet ent eate",
-    "Too ato too nOt enot one totA not anot tOO aNot",
-    "oat itain oat tain nate eate tea anne inant nean",
-    "itant eate anot eat nato inate eat anot tain eat",
-    "nee ene ate ite tent tiet ent ine ene ete ene ate",
-  ];
-  let charIndex = 0;
-  let sentenceIndex = 0;
+  $(document).ready(function () {
+    let keysLower = $("#keyboard-lower-container");
+    let keysUpper = $("#keyboard-upper-container");
+    let sentences = [
+      "ten ate neite ate nee enet ite ate inet ent eate",
+      "Too ato too nOt enot one totA not anot tOO aNot",
+      "oat itain oat tain nate eate tea anne inant nean",
+      "itant eate anot eat nato inate eat anot tain eat",
+      "nee ene ate ite tent tiet ent ine ene ete ene ate",
+    ];
+    let sentencesIndex = 0;
+    let charIndex = 0;
+    let totalWords = 54;
+    let currentSentence = sentences[sentencesIndex];
+    let currentChar = currentSentence[charIndex];
+    let mistakes = 0;
+    let startTime = Date.now();
 
-  let currentSentence = sentences[0];
-  let currentChar = currentSentence[0];
-  let nextChar = currentSentence[charIndex];
-  // Upper-keyboard hidden on load-up
-  $("#keyboard-upper-container").hide();
+    $("#sentence").append(currentSentence);
+    $("#target-letter").append(currentChar);
 
-  //Shift keydown will toggle upper/lower keyboard fuction
-  $(document).keydown(function (shift) {
-    if (shift.which === 16) { // Tuggles upper keyboard with shift-key down
-      $("#keyboard-lower-container").hide();
-      $("#keyboard-upper-container").show();
-    }
-  });
-  $(document).keyup(function (shift) { // Tuggles lower keyboard with shift-key up
-    if (shift.which === 16) {
-      $("#keyboard-lower-container").show();
-      $("#keyboard-upper-container").hide();
+    keysUpper.hide();
 
-    }
-  });
+    $(document).keypress(function (event) {
+      $("#" + event.keyCode).addClass("style"); // Highlight .style property set
 
-  // The event keypress only counts  regluar keys.
-  $(document).keypress(function (e) {
-    console.log(currentSentence.charAt(charIndex))
-    if (currentSentence.charCodeAt(charIndex) === e.keyCode) { // 
-      let currentKey = $("#" + e.charCode);
-      let styleObject = $(currentKey).prop("style");
-      $(currentKey).css({ "background-color": "yellow" });
-      $(document).keyup(function(){
-        styleObject.removeProperty("background-color");
-        
-
-      })
-      $("#yellow-block").css("left", "+=20");
+      if (event.keyCode === currentSentence.charCodeAt(charIndex)) {
+        $("#feedback").append('<div class="glyphicon glyphicon-ok"></div>'); //Correct key pressed = check
+      } else {
+        $("#feedback").append('<div class="glyphicon glyphicon-remove"></div>'); //Incorrect key pressed = X
+        mistakes++;
+      }
       charIndex++;
-      nextChar = currentSentence[charIndex];
-      $("#target-letter").append(nextChar);
-    // } else if (e.which === charIndex.charCodeAt(0)) {
-    //   $('#feedback').append('<span class="glyphicon glyphicon-ok"></span>');
-    // };
+      currentChar = currentSentence[charIndex];
 
+      $("#target-letter").text(currentChar);
+      $("#yellow-block").css("left", "+=17.5px");
 
-    }
+      if (charIndex === currentSentence.length) {
+        sentencesIndex++;
+        if (sentencesIndex === sentences.length) {
+          $("body").off();
+          let endTime = Date.now();
+          let minutes = (endTime - startTime) * 60000;
+          let wpm = Math.round((totalWords / minutes) - (mistakes * 2));
+
+          $(".glyphicon").remove();
+          let results = "Congrats! You typed " + wpm + " words per minute.";
+          $("#feedback").append(results);
+          $("#feedback").append('<button class="btn btn-info">Play Again</button>');
+          $(".btn").on();
+          $(".btn").click(function () {
+            location.reload();
+          });
+          return;
+        }
+        currentSentence = sentences[sentencesIndex];
+        $("#sentence").text(currentSentence);
+        charIndex = 0;
+        currentChar = currentSentence[charIndex];
+        $("#target-letter").text(currentChar);
+        $("#yellow-block").css("left", "0");
+        $(".glyphicon").remove();
+      }
+    });
+
+    $(document).keyup(function (event) {
+      if (event.key == "Shift") {
+        $("#keyboard-upper-container").hide();
+        $("#keyboard-lower-container").show();
+      } else {
+        $("#" + event.key.charCodeAt(0)).css("background-color", "#yellow");
+      }
+    });
+
+    $(document).keydown(function (event) {
+      //Shit-key toggles upper/lowwer keyboard
+      if (event.keyCode === 16) {
+        keysUpper.show();
+        keysLower.hide();
+      }
+    });
+    $(document).keyup(function (event) {
+      if (event.keyCode === 16) {
+        keysUpper.hide();
+        keysLower.show();
+      }
+      $(".style").removeClass("style"); // Returns Key to initial color
+    });
   });
-
-
-  
-
-  $("#sentence").append(currentSentence);
-  $("#target-letter").append(currentChar);
 });
